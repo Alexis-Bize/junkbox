66c2c6c2ebceaa0252e3096cfff625d334c3bef8
Alexis-Bize <alexis.bize@gmail.com>
2020-03-22 19:25:11 +0100

    WIP - Frontend

--- www/frontend/src/pages/index.tsx
+++ www/frontend/src/pages/index.tsx
@@ -1,26 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+
+import {
+	DeleteOutlined,
+	CopyOutlined,
+	ReloadOutlined
+} from '@ant-design/icons';
 
 import {
 	Descriptions,
 	PageHeader,
 	Layout,
 	Input,
-	Badge,
 	List,
 	Avatar,
 	Button,
 	Row,
 	Col
 } from 'antd';
-
-import {
-	DeleteOutlined,
-	MailOutlined,
-	CopyOutlined,
-	ReloadOutlined,
-	GithubOutlined,
-	TwitterOutlined
-} from '@ant-design/icons';
 
 const getSenderInitials = (from: string) => {
 	const match = from.split('<')[0].match(/[a-zA-Z0-9]+/g) || [];
@@ -49,74 +45,92 @@ const Index = () => {
 		count: number;
 	}>(null);
 
-	useEffect(() => {
-		const requestMailbox = async () => {
-			const result = await fetch('/api/mailbox');
+	const pullMailbox = useCallback(async () => {
+		if (mailbox !== null) {
+			const qs = `box=${mailbox.box}&hash=${mailbox.hash}`;
+			const result = await fetch(`/api/mailbox/pull?${qs}`);
 			const json = await result.json();
-			setMailbox(json);
-		};
+			setData(json);
+		}
+	}, [mailbox]);
 
-		const pullMailbox = async (box: string, hash: string) => {
-			if (mailbox !== null) {
-				const qs = `box=${box}&hash=${hash}`;
-				const result = await fetch(`/api/mailbox/pull?${qs}`);
-				const json = await result.json();
-				setData(json);
-			}
-		};
+	const requestMailbox = useCallback(async () => {
+		const result = await fetch('/api/mailbox');
+		const json = await result.json();
+		setMailbox(json);
+		setData(null);
+	}, []);
 
+	useEffect(() => {
 		if (mailbox === null) requestMailbox();
-		else if (data === null) pullMailbox(mailbox.box, mailbox.hash);
-	}, [mailbox, data]);
+		else if (data === null) pullMailbox();
+	}, [mailbox, data, requestMailbox, pullMailbox]);
 
 	return (
 		<Layout id="junkbox-pages-index" style={{ height: '100vh' }}>
-			<Layout.Sider id="junkbox-components-sider">
-				<Layout.Header style={{ backgroundColor: '#FFCC66' }}>
-					Junkbox
-				</Layout.Header>
-				<TwitterOutlined />
-				<GithubOutlined />
-			</Layout.Sider>
+			<Layout.Header
+				id="junkbox-components-header"
+				style={{
+					height: 'auto',
+					paddingTop: '16px',
+					paddingBottom: '16px'
+				}}>
+				<div
+					className="mail-details"
+					style={{
+						textAlign: 'center',
+						maxWidth: '620px',
+						margin: '0 auto'
+					}}>
+					<div
+						style={{
+							fontSize: '20px',
+							fontWeight: 'bold',
+							color: 'white'
+						}}>
+						Your temporary{' '}
+						<span style={{ color: '#FFCC66' }}>
+							Junkbox <DeleteOutlined />
+						</span>{' '}
+						email address
+					</div>
+					<Input
+						addonAfter={
+							<div>
+								<Button type="dashed" icon={<CopyOutlined />} />{' '}
+								<Button
+									onClick={() =>
+										mailbox !== null
+											? pullMailbox()
+											: void 0
+									}
+									type="default"
+									icon={<ReloadOutlined />}
+								/>{' '}
+								<Button
+									onClick={requestMailbox}
+									type="danger"
+									icon={<DeleteOutlined />}
+								/>
+							</div>
+						}
+						type="email"
+						autoComplete="off"
+						size="large"
+						value={
+							mailbox !== null
+								? [mailbox.box, mailbox.domain].join('@')
+								: 'Loading...'
+						}
+					/>
+				</div>
+			</Layout.Header>
 			<Layout.Content
 				id="junkbox-components-mails"
 				style={{ padding: '8px' }}>
-				<Row gutter={[8, 8]}>
+				<Row gutter={[8, 0]}>
 					<Col span={8}>
 						<Layout.Content id="junkbox-components-mails-list">
-							<Input
-								size="large"
-								addonBefore={
-									<Badge
-										count={data?.count || 0}
-										overflowCount={10}>
-										<MailOutlined />
-									</Badge>
-								}
-								addonAfter={
-									<div>
-										<Button
-											type="default"
-											icon={<CopyOutlined />}
-										/>{' '}
-										<Button
-											type="default"
-											icon={<ReloadOutlined />}
-										/>{' '}
-										<Button
-											type="danger"
-											icon={<DeleteOutlined />}
-										/>
-									</div>
-								}
-								value={
-									mailbox !== null
-										? [mailbox.box, mailbox.domain].join(
-												'@'
-										  )
-										: 'Loading...'
-								}
-							/>
 							<List
 								itemLayout="horizontal"
 								dataSource={data === null ? [] : data.items}
@@ -126,7 +140,7 @@ const Index = () => {
 										style={{
 											backgroundColor: 'white',
 											padding: '16px',
-											marginTop: '8px'
+											marginBottom: '8px'
 										}}>
 										<List.Item.Meta
 											avatar={
@@ -175,7 +189,8 @@ const Index = () => {
 											  ].join('@')}>`}
 									</Descriptions.Item>
 									<Descriptions.Item label="Date">
-										Monday 5th October 2020 11:56PM
+										{new Date().toLocaleDateString()} -
+										{new Date().toLocaleTimeString()}
 									</Descriptions.Item>
 								</Descriptions>
 							</PageHeader>

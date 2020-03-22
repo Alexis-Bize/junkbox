282eb048d5265c82fd3abceaefaf542de04f8cd0
Alexis-Bize <alexis.bize@gmail.com>
2020-03-22 12:21:37 +0100

    Prevent env in now lamba

--- www/backend/src/environment.ts
+++ www/backend/src/environment.ts
@@ -1,6 +1,8 @@
 import * as dotenv from 'dotenv';
 import { join } from 'path';
 
-if (process.env.NODE_ENV === 'development') {
-	dotenv.config({ path: join(__dirname, '../../../.env-development') });
-} else dotenv.config({ path: join(__dirname, '../../../.env') });
+if (process.env.NOW_LAMBA !== 'yes') {
+	if (process.env.NODE_ENV === 'development') {
+		dotenv.config({ path: join(__dirname, '../../../.env-development') });
+	} else dotenv.config({ path: join(__dirname, '../../../.env') });
+}

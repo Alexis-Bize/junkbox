f1a8b6ab0231a08acf8292f894583c75ea4d3896
Alexis-Bize <alexis.bize@gmail.com>
2020-03-30 10:24:42 +0200

    Renamed .env local (dev) file

--- www/backend/src/environment.ts
+++ www/backend/src/environment.ts
@@ -3,6 +3,8 @@ import { join } from 'path';
 
 if (process.env.NOW_LAMBA !== 'yes') {
 	if (process.env.NODE_ENV === 'development') {
-		dotenv.config({ path: join(__dirname, '../../../.env-development') });
+		dotenv.config({
+			path: join(__dirname, '../../../.env.development.local')
+		});
 	} else dotenv.config({ path: join(__dirname, '../../../.env') });
 }
